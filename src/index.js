8edf63a83b658d209b7614f5998d4dc393f53a62
Bui Duc Hung <hungbd@innotech-vn.com>
2021-04-27 17:37:42 +0700

    Refactor code: create routes, app/controller

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
-const path = require('path')
+const path = require('path');
+const route = require('./routes');
 
 const app = express();
 const port = 3000;  
 
 //
 app.use(express.static(path.join(__dirname, 'public'))); 
+//body-parser: middleware doc du lieu tu form data hay query parameter chuyen thanh kieu obj
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 
 //HTTP logger
 app.use(morgan('combined'));
@@ -19,13 +25,8 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-//Routes
-app.get('/', (req, res) => {
-    res.render('home');
-});
-app.get('/news', (req, res) => {
-    res.render('news');
-});
+//Routes initial
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
